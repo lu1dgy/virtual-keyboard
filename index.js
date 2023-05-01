b1ddf714ea433c5c3bbf5d1e90809078e66e2cea
alex <lapkesa@gmail.com>
2023-05-01 18:30:56 +0300

    feat:add styles for unique buttons

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ import TextArea from './components/TextArea/index.js';
 // eslint-disable-next-line import/extensions
 import { keyboardLayout } from './utils/constants.js';
 
-const textarea = new TextArea(40).generate();
+const textarea = new TextArea(50).generate();
 const keyboard = new Keyboard(keyboardLayout, textarea);
 keyboard.render(document.body);
